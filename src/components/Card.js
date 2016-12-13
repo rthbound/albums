1b25010739a27e23c7694cfc0096cd5dcc8f7f38
Ryan T. Hosford <tad.hosford@gmail.com>
2016-12-13 15:45:03 -0600

    Styling.

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,15 @@ import { View } from 'react-native';
 
 const Card = (props) => {
   return (
-      <View>
+      <View style={styles.containerStyle}>
         {props.children} 
       </View>
   );
 };
 
 const styles = {
-  constainerStyle: {
-    boderWidth:        1,
+  containerStyle: {
+    borderWidth:        1,
     borderRadius:      2,
     borderColor:       '#ddd',
     borderBottomWidth: 0,
