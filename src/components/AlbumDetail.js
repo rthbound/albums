21b1d0779f9d213f81ae10301e800952e9b224be
Ryan T. Hosford <tad.hosford@gmail.com>
2016-12-13 15:43:27 -0600

    Wrapped in cards... whoops missed styles apparently

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Card from './Card';
 
 const AlbumDetail = (props) => {
   return (
-    <View>
+    <Card>
       <Text
         key={props.album.title}>
         {props.album.title}
       </Text>
-    </View>
+    </Card>
   )
 }
 
