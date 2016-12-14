dc9df70d0341f0ff0c1854b813202b5a75c3b5aa
Ryan T. Hosford <tad.hosford@gmail.com>
2016-12-13 18:20:40 -0600

    Buttons and lists, and structure and... just wow

--- index.ios.js
+++ index.ios.js
@@ -2,16 +2,10 @@ import React                 from 'react';
 import { View, AppRegistry } from 'react-native';
 
 import Header    from './src/components/header';
-import AlbumList from './src/components/AlbumList'
-
-
-// Outputs list of component names and src to be used in HTML head
-// Builtin components (like amp-img) don't add to the list, but extensions (like amp-analytics) will
-// Ex: [ { customElement: 'amp-analytics', src: 'https://cdn.ampproject.org/v0/amp-analytics-0.1.js' } ]
-// console.log(ampScripts);
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View>
--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,19 +1,66 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    headerContentStyle,
+    headerTextStyle,
+    imageStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle
+  } = styles;
   return (
     <Card>
+
       <CardSection>
-        <Text
-          key={props.album.title}>
-          {props.album.title}
-        </Text>
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }}/>
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection>
+
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }}/>
+      </CardSection>
+
+      <CardSection>
+        <Button />
+      </CardSection>
+
     </Card>
   )
+}
+
+const styles = {
+  headerContentStyle: {
+    flexDirection:  "column",
+    justifyContent: "space-around"
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  imageStyle: {
+    height: 300,
+    flex:   1,
+    width:  null
+  },
+  thumbnailStyle: {
+    width: 50,
+    height: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: "center",
+    alignItems:     "center",
+    marginLeft:     10,
+    marginRight:    10
+  }
 }
 
 export default AlbumDetail;
